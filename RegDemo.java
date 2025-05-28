9d67d9eb7d1634843a7047709a9851a30d7f1f26
Jasmin Leonrodriguez <jleonrodriquez@student.sdccd.edu>
2025-05-28 12:03:47 -0700

    updated Java docs

--- RegDemo.java
+++ RegDemo.java
@@ -1,13 +1,33 @@
 
 /**
- * Write a description of class RegDemo here.
+ * The RegDemo class serves as the driver for the car registration project.
+ * It demonstrates the full registration processing flow by using the 
+ * RegistrationMethods class to:
+ * 
+ * <ul>
+ *   <li>Prompt for file names</li>
+ *   <li>Load and process car owner data</li>
+ *   <li>Identify overdue and almost-due registrations</li>
+ *   <li>Write results to an output file</li>
+ * </ul>
+ * 
+ * This class represents the entry point of the DMV-style registration program.
  *
  * @author Jasmin Leonrodriguez, jleonrodriquez@student.sdccd.edu
- * @version v1.0
- * @since 5/22/2025
+ * @version v1.1
+ * @since 5/28/2025
  */
 public class RegDemo
 {
+    /**
+     * The main method drives the car registration application.
+     * It prompts the user for input/output file paths, reads and processes data,
+     * identifies overdue and soon-to-be-due registrations, and writes formatted
+     * output to a report file.
+     *
+     * @param args Command-line arguments (not used)
+     * @throws Exception if any I/O or parsing error occurs during processing
+     */
     public static void main(String[] args) throws Exception
     {
         int size = 0;
